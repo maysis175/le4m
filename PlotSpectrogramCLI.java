a15c0617036d4dcd6c031249656a88ad3b748c7a
TAKII KENSUKE <takii.kensuke.26z@st.kyoto-u.ac.jp>
2018-12-27 16:35:03 +0900

    task1 succeeded

--- PlotSpectrogramCLI.java
+++ PlotSpectrogramCLI.java
@@ -18,6 +18,8 @@ import javafx.scene.Scene;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 import javafx.scene.image.WritableImage;
@@ -86,6 +88,60 @@ public final class PlotSpectrogramCLI extends Application {
         final double nyquist = sampleRate * 0.5;
         stream.close();
         
+        final LineChartWithSpectrogram<Number, Number> chart_sgram =
+                makeSpectrogramChart(cmd, sampleRate, nyquist, waveform);
+        final LineChart<Number, Number> chart_ff =
+                makeFundFreqChart(cmd, sampleRate, nyquist, waveform);
+        final LineChartWithSpectrogram<Number, Number> chart_chromagram =
+                makeChromagramChart(cmd, sampleRate, nyquist, waveform);
+        final LineChart<Number, Number> chart_chord =
+                makeChordChart(cmd, sampleRate, nyquist, waveform);
+        
+        // シーングラフ作成
+        HBox root = new HBox();
+        VBox left = new VBox(2.0);
+        VBox right = new VBox(2.0);
+        root.getChildren().addAll(left, right);
+        
+        left.getChildren().add(chart_sgram);
+        right.getChildren().add(chart_ff);
+        left.getChildren().add(chart_chromagram);
+        right.getChildren().add(chart_chord);
+        
+        /* グラフ描画 */
+        final Scene scene = new Scene(root, 1600, 1200);
+        scene.getStylesheets().add("le4music.css");
+        /*final Scene scene2 = new Scene(chart_ff, 800, 600);
+        scene2.getStylesheets().add("le4music.css");
+        final Scene scene_chroma = new Scene(chart_chromagram, 800, 600);
+        scene_chroma.getStylesheets().add("le4music.css");
+        final Scene scene_chord = new Scene(chart_chord, 800, 600);
+        scene_chord.getStylesheets().add("le4music.css");
+        
+        /* ウィンドウ表示 */
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(getClass().getName());
+        /*secondStage.setScene(scene2);
+        secondStage.setTitle(getClass().getName());
+        secondStage.setX(900); secondStage.setY(100);
+        chromaStage.setScene(scene_chroma);
+        chromaStage.setTitle(getClass().getName());
+        chromaStage.setX(100); chromaStage.setY(700);
+        chordStage.setScene(scene_chord);
+        chordStage.setTitle(getClass().getName());
+        chordStage.setX(900); chordStage.setY(700);*/
+        
+        primaryStage.show();
+        /*secondStage.show();
+        chromaStage.show();
+        chordStage.show();*/
+    }
+    
+    // スペクトログラム＆チャート作成
+    public LineChartWithSpectrogram<Number, Number> makeSpectrogramChart(CommandLine cmd,
+                                                                     double sampleRate,
+                                                                     double nyquist,
+                                                                     double[] waveform){
         /* 窓関数とFFTのサンプル数 */
         final double frameDuration = 
                 Optional.ofNullable(cmd.getOptionValue("frame"))
@@ -141,13 +197,342 @@ public final class PlotSpectrogramCLI extends Application {
         chart.setCreateSymbols(false);
         chart.setLegendVisible(false);
         
-        /* グラフ描画 */
-        final Scene scene = new Scene(chart, 800, 600);
-        scene.getStylesheets().add("le4music.css");
+        return chart;
+    }
+    
+    // 基本周波数＆チャート作成
+    public LineChart<Number, Number> makeFundFreqChart(CommandLine cmd,
+                                                                     double sampleRate,
+                                                                     double nyquist,
+                                                                     double[] waveform){
+        // 1フレームの長さ(sec)とフレームシフト幅(sec)
+        final double frameDuration = Le4MusicUtils.frameDuration;   // 0.2
+        final double shiftDuration = frameDuration / 8.0;           // 0.025
         
-        /* ウィンドウ表示 */
-        primaryStage.setScene(scene);
-        primaryStage.setTitle(getClass().getName());
-        primaryStage.show();
+        /* 1フレームとシフトのサンプル数 */
+        final int frameSize = (int)Math.round(frameDuration * sampleRate);  // 3200 per sec
+        final int shiftSize = (int)Math.round(shiftDuration * sampleRate);  // 400 per sec
+        
+        /* 基本周波数計算 */
+        int shiftSum = (int)Math.round((waveform.length - frameSize) / shiftSize);
+        double[] fundFreq = new double[shiftSum];
+        double[] ac = new double[shiftSum];
+        double ac_buf = 0;
+        for(int i = 0; i < shiftSum; i++){
+            ac_buf = 0;
+            for(int t = 10; t < (int)Math.round(frameSize-1); t++){
+                ac[i]=0;
+                for(int j = 0; j < frameSize - 1; j++){
+                    if(i * shiftSize + j + t >= waveform.length){
+                        ac[i] += 0;
+                    }else{
+                        double sig_0 = waveform[i * shiftSize + j];
+                        double sig_1 = waveform[i * shiftSize + j + t];
+                        
+                        // 小さい音ははじく
+                        if(sig_0 < 0.15 || sig_1 < 0.15) sig_0 = 0;
+                        ac[i] += sig_0 * sig_1;
+                    }
+                }
+                if(ac_buf < ac[i]){
+                    ac_buf = ac[i]; fundFreq[i] = t;
+                }
+            }
+        }
+        
+        /* データ系列を作成 */
+        final ObservableList<XYChart.Data<Number, Number>> data =
+                IntStream.range(0, fundFreq.length)
+                    .mapToObj(i -> new XYChart.Data<Number, Number>(i / sampleRate * shiftSize, fundFreq[i]))
+                    .collect(Collectors.toCollection(FXCollections::observableArrayList));
+        
+        /* データ系列に名前をつける */
+        final XYChart.Series<Number, Number> series = new XYChart.Series<>("Fundamental Frequency", data);
+        
+        /* x軸を作成 */
+        final double duration = (waveform.length - 1) / sampleRate;
+        final NumberAxis xAxis = new NumberAxis("Time (seconds)", 0.0, duration,
+                                                Le4MusicUtils.autoTickUnit(duration));
+        xAxis.setAnimated(false);
+        
+        /* y軸を作成 */
+        final double freqLowerBound =
+                Optional.ofNullable(cmd.getOptionValue("freq-lo"))
+                        .map(Double::parseDouble)
+                        .orElse(0.0);
+        if(freqLowerBound < 0.0)
+            throw new IllegalArgumentException(
+                "freq-lo must be non-negatice: " + freqLowerBound
+            );
+        final double freqUpperBound =
+                Optional.ofNullable(cmd.getOptionValue("freq-up"))
+                        .map(Double::parseDouble)
+                        .orElse(nyquist);
+        if(freqUpperBound <= freqLowerBound)
+            throw new IllegalArgumentException(
+                "freq-up must be larger than freq-lo: " +
+                "freq-lo = " + freqLowerBound + ", freq-up = " + freqUpperBound
+            );
+        final NumberAxis yAxis = new NumberAxis("Frequency (Hz)", freqLowerBound, 500,
+                                                Le4MusicUtils.autoTickUnit(500 - freqLowerBound));
+        yAxis.setAnimated(false);
+        
+        /* チャートを作成 */
+        final LineChart<Number, Number> chart = new LineChart<>(xAxis, yAxis);
+        chart.setTitle("Fundamental Frequency");
+        chart.setCreateSymbols(false);
+        chart.getData().add(series);
+        
+        return chart;
+    }
+    
+    // クロマグラム＆チャート作成
+    public LineChartWithSpectrogram<Number, Number> makeChromagramChart(CommandLine cmd,
+                                                                           double sampleRate,
+                                                                           double nyquist,
+                                                                           double[] waveform){
+        // 1フレームの長さ(sec)とフレームシフト幅(sec)
+        final double frameDuration = Le4MusicUtils.frameDuration;   // 0.2
+        final double shiftDuration = frameDuration / 8.0;           // 0.025
+        
+        // クロマベクトル作成
+        double[][] chromaVec = makeChromaVector(sampleRate, waveform);
+        
+        // X軸を作成 
+        final double duration = (chromaVec.length - 1) * shiftDuration;
+        final NumberAxis xAxis = new NumberAxis("Time (seconds)", 0.0, duration,
+                                                Le4MusicUtils.autoTickUnit(duration));
+        xAxis.setAnimated(false);
+        
+        // Y軸を作成 
+        final NumberAxis yAxis = new NumberAxis("Chromagram (0-12)", 0.0, 12,
+                                                Le4MusicUtils.autoTickUnit(12));
+        yAxis.setAnimated(false);
+        
+        // chチャートを作成 
+        final LineChartWithSpectrogram<Number, Number> chart =
+                new LineChartWithSpectrogram<>(xAxis, yAxis);
+        chart.setParameters(chromaVec.length, 12, 12);
+        chart.setTitle("Chromagram");
+        Arrays.stream(chromaVec).forEach(chart::addSpecLog);
+        chart.setCreateSymbols(false);
+        chart.setLegendVisible(false);
+        
+        return chart;
+    }
+    
+    // コード進行＆チャート作成
+    public LineChart<Number, Number> makeChordChart(CommandLine cmd,
+                                                      double sampleRate,
+                                                      double nyquist,
+                                                      double[] waveform){
+        // クロマベクトル作成
+        double[][] chromaVec = makeChromaVector(sampleRate, waveform);
+        
+        // 和音らしさ
+        // like_chord[i][j] : フレーム i の和音 j らしさ
+        // 0 <= j <= 11 : C, C#, ... , B Maj
+        // 12 <= j <= 23 : C, C#, ... , B Min
+        double[][] like_chord = new double[chromaVec.length][(12 * 2)];
+        double a_root = 1.0, a_3rd = 0.5, a_5th = 0.8;
+        for(int i = 0; i < chromaVec.length; i++){
+            for(int j = 0; j < 12 * 2; j++){
+                if(j < 12){
+                    like_chord[i][j] = a_root * chromaVec[i][j]
+                                     + a_3rd  * chromaVec[i][(j + 4) % 12]
+                                     + a_5th  * chromaVec[i][(j + 7) % 12];
+                }else if(j < 24){
+                    like_chord[i][j] = a_root * chromaVec[i][j % 12]
+                                     + a_3rd  * chromaVec[i][(j + 3) % 12]
+                                     + a_5th  * chromaVec[i][(j + 7) % 12];
+                }
+            }
+        }
+        
+        // 和音らしさをグラフ化
+        double[] like_graph = new double[like_chord.length];
+        for(int i = 0; i < like_graph.length; i++){
+            like_graph[i] = argmax(like_chord[i]);
+        }
+        
+        // データ系列を作成 
+        final ObservableList<XYChart.Data<Number, Number>> data =
+                IntStream.range(0, like_graph.length)
+                    .mapToObj(i -> new XYChart.Data<Number, Number>(i * sampleRate / waveform.length / 5, like_graph[i]))
+                    .collect(Collectors.toCollection(FXCollections::observableArrayList));
+        
+        // データ系列に名前をつける 
+        final XYChart.Series<Number, Number> series = new XYChart.Series<>("Chord Progression", data);
+        
+        /* x軸を作成 */
+        final double duration = (waveform.length - 1) / sampleRate;
+        final NumberAxis xAxis = new NumberAxis("Time (seconds)", 0.0, duration,
+                                                Le4MusicUtils.autoTickUnit(duration));
+        xAxis.setAnimated(false);
+        
+        /* y軸を作成 */
+        final NumberAxis yAxis = new NumberAxis("Chord (0-24)", 0, 24,
+                                                Le4MusicUtils.autoTickUnit(24));
+        yAxis.setAnimated(false);
+        
+        // チャートを作成 
+        final LineChart<Number, Number> chart = new LineChart<>(xAxis, yAxis);
+        chart.setTitle("Chord Progression");
+        chart.setCreateSymbols(false);
+        chart.getData().add(series);
+        
+        return chart;
+    }
+    
+    
+    // クロマベクトル作成
+    public double[][] makeChromaVector(double sampleRate,
+                                         double[] waveform){
+        // 1フレームの長さ(sec)とフレームシフト幅(sec)
+        final double frameDuration = Le4MusicUtils.frameDuration;   // 0.2
+        final double shiftDuration = frameDuration / 8.0;           // 0.025
+        
+        /* 窓関数とFFTのサンプル数 */
+        final int frameSize = (int)Math.round(frameDuration * sampleRate);
+        final int fftSize = 1 << Le4MusicUtils.nextPow2(frameSize);
+        final int fftSize2 = (fftSize >> 1) + 1;
+        
+        /* シフトのサンプル数 */
+        final int shiftSize = (int)Math.round(shiftDuration * sampleRate);
+        
+        /* 窓関数を求め正規化する */
+        final double[] window = MathArrays.normalizeArray(
+            Arrays.copyOf(Le4MusicUtils.hanning(frameSize), fftSize), 1.0
+        );
+        
+        /* 短時間フーリエ変換本体 */
+        final Stream<Complex[]> spectrogram =
+                Le4MusicUtils.sliding(waveform, window, shiftSize)
+                             .map(frame -> Le4MusicUtils.rfft(frame));
+        
+        /* 複素スペクトログラムを対数振幅スペクトログラムに */
+        final double[][] specLog =
+                spectrogram.map(sp -> Arrays.stream(sp)
+                                            .mapToDouble(c -> c.abs())
+                                            .toArray())
+                           .toArray(n -> new double[n][]);
+        
+        // クロマベクトル
+        double[][] chromaVec = new double[specLog.length][17];
+        for(int i = 0; i < specLog.length; i++){
+            for(int j = 0; j < 17; j++){
+                if(j < 12){
+                    chromaVec[i][j] = -0.15 / chromaPower(specLog[i], j, sampleRate * 0.5, fftSize2);
+                    if(chromaVec[i][j] == 0) chromaVec[i][j] = Integer.MIN_VALUE;
+                }else{
+                    chromaVec[i][j] = 0;
+                }
+            }
+        }
+        
+        return chromaVec;
+    }
+    
+    // 認識したメロディ＆チャートの表示
+    /*public LineChart<Number, Number> makeMelody(CommandLine cmd,
+                                                      double sampleRate,
+                                                      double nyquist,
+                                                      double[] waveform){
+        // 1フレームの長さ(sec)とフレームシフト幅(sec)
+        final double frameDuration = Le4MusicUtils.frameDuration;   // 0.2
+        final double shiftDuration = frameDuration / 8.0;           // 0.025
+        
+        /* 窓関数とFFTのサンプル数 
+        final int frameSize = (int)Math.round(frameDuration * sampleRate);
+        final int fftSize = 1 << Le4MusicUtils.nextPow2(frameSize);
+        final int fftSize2 = (fftSize >> 1) + 1;
+        
+        /* シフトのサンプル数 
+        final int shiftSize = (int)Math.round(shiftDuration * sampleRate);
+        
+        /* 窓関数を求め正規化する 
+        final double[] window = MathArrays.normalizeArray(
+            Arrays.copyOf(Le4MusicUtils.hanning(frameSize), fftSize), 1.0
+        );
+        
+        /* 短時間フーリエ変換本体 
+        final Stream<Complex[]> spectrogram =
+                Le4MusicUtils.sliding(waveform, window, shiftSize)
+                             .map(frame -> Le4MusicUtils.rfft(frame));
+        
+        /* 複素スペクトログラムを対数振幅スペクトログラムに 
+        final double[][] specLog =
+                spectrogram.map(sp -> Arrays.stream(sp)
+                                            .mapToDouble(c -> c.abs())
+                                            .toArray())
+                           .toArray(n -> new double[n][]);
+        
+        // 基本周波数の候補集合
+        double[] ffCandSet = new double[(72 - 35) * 10];
+        for(int i = 0; i < ffCandSet.length; i++){
+            ffCandSet[i] = 36 + i * 0.1;
+        }
+        
+        // fundFreq[i][j] : フレーム i の音階 j らしさ
+        // ノートナンバー = 36 + j * 0.1 (半音を10等分)
+        double[][] fundFreq = new double[specLog.length][ffCandSet.length];
+        for(int i = 0; i < fundFreq.length; i++){
+            for(int j = 0; j < ffCandSet.length; j++){
+                
+            }
+        }
+    }*/
+    
+    // ノートナンバーから周波数への変換
+    public double n_to_f(double noteNum){
+        return (440 * Math.pow(2, ((noteNum-69)/12)));
+    }
+    
+    // 周波数からノートナンバーへの変換
+    public int f_to_n(double freq){
+        return (int)Math.round(12 * Math.log(freq / 440) / Math.log(2) + 69);
+    }
+    
+    // 0 〜 2048 の arrnum から対応する周波数を求める
+    public double freq(double nyquist, int fftSize, int arrnum){
+        return nyquist / fftSize * arrnum;
+    }
+    
+    public double argmax(double[] arr){
+        double max = arr[0];
+        double argmax = 0;
+        for(int i = 0; i < arr.length-1; i++){
+            if(max < arr[i+1]){
+                max = arr[i+1];
+                argmax = i + 1;
+            }
+        }
+        return argmax;
+    }
+    
+    // 各音名のパワーを求める
+    // toneName は 0 〜 12 で C, C#, ... B に対応
+    public double chromaPower(double[] spec, int toneName, double nyquist, int fftSize){
+        double halfhalftone = Math.pow(2.0, 1.0/30.0);
+        double powerSum = 0;
+        double[] toneFreq = new double[12];
+        for(int i = 0; i < 12 ; i++){
+            toneFreq[i] = n_to_f(60+i);
+        }
+        
+        double[] baseFreq = new double[5];
+        int div = 1;
+        for(int i = 0; i <= 4; i++){
+            baseFreq[i] = toneFreq[toneName] * Math.pow(2.0, (double)(i-2));
+            for(int j = 0; j < spec.length; j++){
+                double frameFreq = spec[j];
+                if(freq(nyquist, fftSize, j) >= baseFreq[i] / halfhalftone
+                        && freq(nyquist, fftSize, j) <= baseFreq[i] * halfhalftone){
+                    powerSum += frameFreq;
+                    div++;
+                }
+            }
+        }
+        return powerSum / div;
     }
 }
